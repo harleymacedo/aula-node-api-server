f8e47b087d0ad85ee15fbc7b7c99885aa4fcc12a
Harley Macedo <harley.ip@gmail.com>
2020-09-01 08:10:21 -0300

    Busca por área adicionada

--- app.js
+++ app.js
@@ -10,17 +10,18 @@ app.use(cors({
 app.get('/api/:termo', function(req, res) {
     //Dados mockados
     let profs = [
-        {nome: 'Harley', instituicao: 'IFCE', area: 'Desenvolvimento Web e Móvel',campus: 'Crato'},
-        {nome: 'Ana', instituicao: 'IFCE', area: 'Computação Gráfica',campus: 'Crato'},
-        {nome: 'Talles', instituicao: 'IFCE', area: 'Estrutura de Dados',campus: 'Crato'},
-        {nome: 'Robson', instituicao: 'IFCE', area: 'Inteligência Artificial',campus: 'Crato'},
+        {nome: 'Harley', instituicao: 'IFCE', area: 'Desenvolvimento Web e Móvel', campus: 'Crato'},
+        {nome: 'Ana', instituicao: 'IFCE', area: 'Computação Gráfica', campus: 'Crato'},
+        {nome: 'Talles', instituicao: 'IFCE', area: 'Estrutura de Dados', campus: 'Crato'},
+        {nome: 'Robson', instituicao: 'IFCE', area: 'Inteligência Artificial', campus: 'Crato'},
     ]
 
     //Pesquida de dados usando termo
     let termo = req.params.termo
     let resultado = []
     profs.map( (item) => {
-        if (item.nome === termo) {
+        console.log('Teste')
+        if (item.nome === termo || item.area.search(termo)) {
             resultado.push(item)
         }
     })
